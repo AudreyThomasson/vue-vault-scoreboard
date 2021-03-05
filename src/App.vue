03220b65cb067593f677d4b5be7be10810ec813a
Audrey Thomasson <audrey@macbook-pro.attlocal.net>
2021-03-05 12:34:05 -0600

    working

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,22 @@
     <!-- calling matchupTitle.vue an passing in the objects of team1
     and team2 -->
     <matchup-title :team1="team1" :team2="team2"></matchup-title>
-    <score-card :score="team1.score"></score-card>
-    <score-card :score="team2.score"></score-card>
+    <game-status :team1="team1" :team2="team2"></game-status>
+    <div class="scores">
+      <score-card :score="team1.score" @pointAdded="updateTeamScore(team1, 1)" @pointSubtracted="updateTeamScore(team1, -1)"></score-card>
+      <score-card :score="team2.score" @pointAdded="updateTeamScore(team2, 1)" @pointSubtracted="updateTeamScore(team2, -1)"></score-card>
+    </div>
   </div>
 </template>
 
 <script>
 import MatchupTitle from './components/MatchupTitle.vue';
 import ScoreCard from './components/ScoreCard';
+import GameStatus from './components/GameStatus';
+
 
 export default {
-  components: { MatchupTitle, ScoreCard },
+  components: { MatchupTitle, ScoreCard, GameStatus },
 
   data() {
     return {
@@ -25,13 +30,35 @@ export default {
         name: "Team 2",
         score: 0
       }
-
-    };
+    }
   },
+  methods: {
+    updateTeamScore(team, points) {
+      const updated = team.score + points;
+      if (updated >= 0) {
+        team.score = updated;
+      } 
+    }
+  }
 
-}
+};
 </script>
 
 <style>
+body {
+  background-color: #2c3e50;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: rgba(255, 255, 255, 0.8);
+  margin-top: 60px;
+}
+.scores {
+  display: flex;
+  justify-content: space-evenly;
+}
 
 </style>
--- src/components/MatchupTitle.vue
+++ src/components/MatchupTitle.vue
@@ -1,18 +1,19 @@
 <template>
-    <h1>
-        <span> {{ team1.name}}</span>
-        vs
-        <span> {{ team2.name}}</span>
-    </h1>
-  
+  <h1>
+    <span :class="{ winning: team1.score > team2.score }">{{ team1.name }}</span> 
+    vs 
+    <span :class="{ winning: team2.score > team1.score }">{{ team2.name }}</span>
+  </h1>
 </template>
 
 <script>
 export default {
-    props: ["team1", "team2"],
+  props: ["team1", "team2"],
 }
 </script>
 
 <style>
-
+  .winning {
+    color: goldenrod;
+  }
 </style>
